945afcb0cca4a27aeb85c316a94b3762a816f13f
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2023-04-01 18:46:52 -0500

    add min python version

--- src/run_game.py
+++ src/run_game.py
@@ -348,6 +348,12 @@ if __name__ == '__main__':
     # pygame.display.init()
     # pygame.font.init()
     # pygame.mixer.init()
+
+    MIN_VER = (3, 9)
+
+    if sys.version_info[:2] < MIN_VER:
+        sys.exit("This game requires Python {}.{}.".format(*MIN_VER))
+
     pygame.init()
     clock = pygame.time.Clock()
 
